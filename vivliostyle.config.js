ae601a6422d163c6c2769d297483f97eec5e1810
erukiti <erukiti+github@gmail.com>
2024-12-21 22:25:18 +0900

    fix

--- vivliostyle.config.js
+++ vivliostyle.config.js
@@ -5,9 +5,8 @@ module.exports = {
 	size: "B5", // A4: 教科書サイズ、B5: 最近流行りの小さいサイズの技術書
 	theme: [
 		"@vivliostyle/theme-techbook@^2.0.0", // .css or local dir or npm package. default to undefined.
-		"fonts/theme.css", //必ず先頭に持ってくること
+		"fonts/theme.css",
 	],
-	style: "fonts/theme.css",
 	entry: ["00-preface.md", "chap-erukiti.md", "99-postscript.md"],
 	entryContext: "./manuscripts", // output: [ // path to generate draft file(s). default to '{title}.pdf'
 	//   './output.pdf', // the output format will be inferred from the name.
