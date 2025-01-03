2e8fc785761822afdcfe6f476d96998c75217b74
erukiti <erukiti+github@gmail.com>
2025-01-03 13:40:40 +0900

    新しいテーマとCSSツールキットを追加し、ビルドスクリプトを更新。不要なテーマファイルを削除。

--- vivliostyle.config.mjs
+++ vivliostyle.config.mjs
@@ -1,68 +1,89 @@
 import { VFM } from "@vivliostyle/vfm";
 import rehypeMermaid from "rehype-mermaid";
 
-export default {
-	title: "とにかく楽にMarkdownで本を作りたい！", // populated into `publication.json`, default to `title` of the first entry or `name` in `package.json`.
-	author: "erukiti <erukiti+github@gmail.com>", // default to `author` in `package.json` or undefined.
-	language: "ja", // default to undefined.
-	size: "JIS-B5", // A4: 教科書サイズ、JIS-B5: 最近流行りの小さいサイズの技術書
-	theme: [
-		// "@vivliostyle/theme-base@^2.0.0", // .css or local dir or npm package. default to undefined.
-		"@vivliostyle/theme-techbook@^2.0.0", // .css or local dir or npm package. default to undefined.
-		"./theme-nice-techbook",
-		"./fonts",
-	],
-	entry: [
-		// 表紙
-		// { rel: "cover" },
+console.log(process.argv);
 
-		// 前書き
-		"00-preface.md",
+const isPressReady = process.argv.includes("--press-ready");
 
-		// 目次
-		{ rel: "contents" },
+const def = {
+  title: "vivliostyle-sample", // populated into `publication.json`, default to `title` of the first entry or `name` in `package.json`.
+  author: "erukiti <erukiti+github@gmail.com>", // default to `author` in `package.json` or undefined.
+  language: "ja", // default to undefined.
+  size: "JIS-B5", // A4: 教科書サイズ、JIS-B5: 最近流行りの小さいサイズの技術書
+  theme: [
+    // "@vivliostyle/theme-techbook@^2.0.0", // .css or local dir or npm package. default to undefined.
+    "./fonts",
+    "./theme-nice-techbook2",
+  ],
+  entry: [
+    // 表紙
+    // { rel: "cover" },
 
-		// 本文
-		"chap-setup.md",
-		"chap-markdown.md",
-		"chap-theme.md",
-		"chap-font.md",
-		"chap-toc.md",
-		"chap-mermaid.md",
-		"chap-sample.md",
-		"chap-onestop.md",
-		// 後書き
-		"90-postscript.md",
-		"99-colophon.md",
-	],
-	entryContext: "./src",
+    // 前書き
+    "00-preface.md",
 
-	// output: [ // path to generate draft file(s). default to '{title}.pdf'
-	//   './output.pdf', // the output format will be inferred from the name.
-	//   {
-	//     path: './book',
-	//     format: 'webpub',
-	//   },
-	// ],
-	workspaceDir: ".vivliostyle", // directory which is saved intermediate files.
-	toc: {
-		title: "目次", // title of table of contents. default to 'Contents'.
-		sectionDepth: 2,
-		includeCover: false, // include cover page in table of contents. default to 'false'.
-	},
-	// cover: './cover.png', // cover image. default to undefined.
-	vfm: {
-		// options of VFM processor
-		//   hardLineBreaks: true, // converts line breaks of VFM to <br> tags. default to 'false'.
-		//   disableFormatHtml: true, // disables HTML formatting. default to 'false'.
-	},
-	documentProcessor: (config, metadata) => {
-		return VFM(config, metadata).use(rehypeMermaid, {
-			strategy: "img-png",
-			//
-			mermaidConfig: {
-				fontFamily: "'IBM Plex Serif', 'BIZ UD Mincho', sans-serif",
-			},
-		});
-	},
+    // 目次
+    { rel: "contents" },
+
+    // 本文
+    "chap-setup.md",
+    "chap-markdown.md",
+    "chap-theme.md",
+    "chap-font.md",
+    "chap-toc.md",
+    "chap-mermaid.md",
+    "chap-sample.md",
+    "chap-onestop.md",
+    // 後書き
+    "90-postscript.md",
+    "99-colophon.md",
+  ],
+  entryContext: "./src",
+
+  // output: [ // path to generate draft file(s). default to '{title}.pdf'
+  //   './output.pdf', // the output format will be inferred from the name.
+  //   {
+  //     path: './book',
+  //     format: 'webpub',
+  //   },
+  // ],
+  workspaceDir: ".vivliostyle", // directory which is saved intermediate files.
+  toc: {
+    title: "目次", // title of table of contents. default to 'Contents'.
+    sectionDepth: 2,
+    includeCover: false, // include cover page in table of contents. default to 'false'.
+  },
+  // cover: './cover.png', // cover image. default to undefined.
+  vfm: {
+    // options of VFM processor
+    //   hardLineBreaks: true, // converts line breaks of VFM to <br> tags. default to 'false'.
+    //   disableFormatHtml: true, // disables HTML formatting. default to 'false'.
+  },
+  documentProcessor: (config, metadata) => {
+    return VFM(config, metadata).use(rehypeMermaid, {
+      strategy: "img-png",
+      //
+      mermaidConfig: {
+        fontFamily: "'IBM Plex Serif', 'BIZ UD Mincho', sans-serif",
+      },
+    });
+  },
 };
+
+if (isPressReady) {
+  def.theme = [
+    ...def.theme,
+    // グレースケール印刷可能なPDF
+    "./theme-nice-techbook2/theme-print-pdf.css",
+  ];
+} else {
+  def.theme = [
+    ...def.theme,
+    // オンライン向けのフルカラーPDF
+    "./theme-nice-techbook2/theme-online-pdf.css",
+    "./theme-nice-techbook2/theme-base/css/lib/prism/base.css",
+    "./theme-nice-techbook2/theme-base/css/lib/prism/theme-okaidia.css",
+  ];
+}
+
+export default def;
